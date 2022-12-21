1b0fb02b58f0a41d624f2042424c6d05c86e5c0f
Oleg <starwarser2@gmail.com>
2022-12-22 02:17:48 +0700

    Написали основной код проекта

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Input the data, separated by commas.");
+string[] massive = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);    
+string[] newmassive = new string[massive.Length];                                           
+Console.WriteLine("New array:");
+for (int i = 0; i < massive.Length; i++)                                                    
+{
+    if (massive[i].Length <= 3)                                                             
+    {
+        newmassive[i] = massive[i];
+        Console.Write(newmassive[i] + " ");
+    }    
+}
